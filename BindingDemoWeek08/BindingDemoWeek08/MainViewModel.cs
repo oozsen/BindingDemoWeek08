869f032818521ce2ea5877aaaab5934dd0a7c534
Özgür ÖZŞEN <oozsen@anadolu.edu.tr>
2019-11-19 11:51:40 +0300

    Slider eklendi, BindingContext eklendi.

---
+++ BindingDemoWeek08/BindingDemoWeek08/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BindingDemoWeek08
+{
+    public class MainViewModel : INotifyPropertyChanged
+
+    {
+        string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == value)
+                    return;
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+
+        public string DisplayName => $"Name Entered: {Name}";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
